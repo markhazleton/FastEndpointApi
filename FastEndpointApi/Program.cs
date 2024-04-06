e1cd61b240fa71834cb41ff48f86792348e4ac45
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-04-06 12:39:57 -0500

    Add Swagger Support

--- FastEndpointApi/Program.cs
+++ FastEndpointApi/Program.cs
@@ -1,8 +1,16 @@
 using FastEndpoints;
+using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
-
+builder.Services.SwaggerDocument(o =>
+{
+    o.DocumentSettings = s =>
+    {
+        s.Title = "FastEndpoints API";
+        s.Version = "v1";
+    };
+});
 var app = builder.Build();
-app.UseFastEndpoints();
+app.UseFastEndpoints().UseSwaggerGen();
 app.Run();
